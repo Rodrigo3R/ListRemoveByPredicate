24876d560c79a46e293436749ce78bf2cefb49c6
Rodrigo3R <rodrigo3r7@hotmail.com>
2020-04-17 09:25:15 -0300

    List remove by predicate

---
+++ src/application/Program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		List<String> list = new ArrayList<>();
+
+		list.add("Maria");
+		list.add("Alex");
+		list.add("Bob");
+		list.add("Anna");
+
+		for (String lista : list) {
+			System.out.println(lista);
+		}
+
+		System.out.println("-------------------------------");
+		list.add(2, "Marco");
+
+		for (String lista : list) {
+			System.out.println(lista);
+		}
+
+		list.removeIf(x -> x.charAt(0) == 'M');
+
+		System.out.println("------------------------");
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+
+		System.out.println("-------------------");
+
+		System.out.println("Index of Bob " + list.indexOf("Bob"));
+		System.out.println("Index of Marco " + list.indexOf("Marco"));
+
+		System.out.println("--------------------------------");
+		List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+		for (String b : result) {
+			System.out.println(b);
+		}
+		System.out.println("-------------------");
+
+		String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null);
+		System.out.println(name);
+	}
+
+}
